8d72e081992a01154e2195591ac13b0c1f59d2c3
TechKind <ken_hayakawa@tech-kind.biz>
2022-06-03 14:21:40 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorBasicSample/EditForm/EnumUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace BlazorBasicSample.EditForm
+{
+    public static class EnumUtility
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            .GetName();
+        }
+    }
+}
